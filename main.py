2dcdc3795b2da9b141f16bc975f479738a2ef55b
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-12-23 18:50:10 +0300

    1.0

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import random
 from discord.ext import commands
 
+# Bot token'i token.0 dosyası içine yazıp oradan token değerine okutuyoruz
 f = open("token.0", "r")
 token = f.read()
 f.close()
 
 bot = commands.Bot(command_prefix="#")
+
+# Tüm emojilerin 
 emojis = [":whitemehaha:791189662055137281", ":kodluyoruz1:791195202748022784", ":kodluyoruz2:791195244662095872", ":kodluyoruz3:791195281734893579"]
 
 @bot.event
 async def on_ready():
-    print("Kodluyoruz BOT has landed !")
+    print("Kodluyoruz BOT Aktif Hale Geldi!!")
 
 @bot.event
 async def on_message(message):
