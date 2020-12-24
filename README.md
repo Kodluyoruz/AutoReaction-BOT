1a983d183441d69a25645bb19789dac5bda26b2a
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-12-24 07:01:43 +0300

    FINAL

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # AutoReaction-BOT
 A Discord BOT written in Python to leave the specificed emojis under the new messages, which are newly sent to the specified channel.
+
+> "Kodluyoruz" branch is ready to deploy branch that you can use in your own server
+
+### Requirements
+```
+Windows: pip install -r requirements.txt
+Linux & MacOS: $pip3 install -r requirements.txt
+```
+
+### Steps
+
+```
+1) Create a "token.0" named file and paste your token in
+2) Replace the emoji ID's in the lists with your own server upload emoji ID's
+3) Configure your own "Announcements, Welcome, Introduce yourself" channel ID's
+4) Deploy your BOT
+```
--- welcome.py
+++ welcome.py
@@ -8,29 +8,10 @@ f.close()
 
 bot = commands.Bot(command_prefix="#")
 
-# Tüm emojilerin ID'leri ve isimleri aşağıda belirtilen listedeki şekilde listeye kayd edilmesi yeterlidir
-emojis = [":onay:756165714020270211",
-          ":kodluyoruzlogo:778245370534297610",
-          ":kodluyoruzpurple:778245370694074368",
-          ":kodluyoruzlightblue:778245370525515786",
-          ":kodluyoruzyellow:778245370907721759",
-          ":kodluyoruzaqua:778245370408861716",
-          ":kodluyoruzblue:778245370073448459",
-          ":kodluyoruzpink:778245370621853706",
-          ":kodluyoruzblack:778245370332708904"]
-
 @bot.event
 async def on_ready():
     print("Kodluyoruz BOT Aktif Hale Geldi!!")
     registerChannel = bot.get_channel(755411760386670693)
     await registerChannel.send("Merhabalar! Ben Kodluyoruz BOT! Kayd olabilir miyim lütfen?")
-
-@bot.event
-async def on_message(message):
-    if(message.channel.id == 727896197624234034 or message.channel.id == 727896370845057055):
-        for i in emojis:
-            await message.add_reaction(i)
-    else:
-        pass
-
+    
 bot.run(token)
